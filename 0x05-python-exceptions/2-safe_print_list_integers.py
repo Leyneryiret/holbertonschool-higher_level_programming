f7fc125d439da04bd01a0c0937ac713fc947427f
leyneryiret <4704@holbertonschool.com>
2022-05-23 21:42:52 -0500

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             conta += 1
         except (TypeError, ValueError):
             pass
-        print("")
-        return conta
+    print("")
+    return conta
