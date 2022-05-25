685ae09e5f7b2a332a2c4163a62cce5d5121bc3e
leyneryiret <4704@holbertonschool.com>
2022-05-24 19:01:26 -0500

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,13 @@
 
 
 class Square:
-    """class Square that defines a square by: (based on 0-square.py)"""
+    """class Square that defines a square by: (based on 1-square.py)"""
     def __init__(self, size=0):
         """Initialize Square with size attribute"""
-        try:
-            self.__size = size
-        except TypeError:
-            print(size must be an integer)
-        except ValueError:
-            print(size must be >= 0)
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
