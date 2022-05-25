5c017ceecd58eb1cf436476593ed1de8a011c0c9
leyneryiret <4704@holbertonschool.com>
2022-05-24 22:38:38 -0500

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,18 +44,20 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if any(i < 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
         """Method that prints the square"""
         if self.__size > 0:
-            for a in range(self.__position[1]):
-                print("")
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                for b in range(self.__position[0]):
+                for x in range(self.__position[0]):
                     print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
-                print("")
+                print()
         else:
-            print("")
+            print()
