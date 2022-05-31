619874d43a8a666505f801984e71fb2718e09102
leyneryiret <4704@holbertonschool.com>
2022-05-31 12:53:18 -0500

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -20,9 +20,9 @@ def add_integer(a, b=98):
         int: The addition of a and b.
     """
 
-    if not isinstance(a, (int, float)):
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
