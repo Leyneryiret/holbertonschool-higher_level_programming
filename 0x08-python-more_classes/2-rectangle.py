5c0db705572acfe868faf55c20d6aa305e45ac72
leyneryiret <4704@holbertonschool.com>
2022-05-31 21:04:01 -0500

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,8 +6,8 @@ Define the class Rectangle
 
 class Rectangle:
     """ Class rectangle that defines a
-    rectangle by: (based on 0-rectangle.py)"""
-
+    rectangle by: (based on 1-rectangle.py)"""
+    
     def __init__(self, width=0, height=0):
         """ Initialize the rectangle """
         self.width = width
@@ -21,7 +21,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Method that sets the width of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -35,8 +35,18 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Method that sets the height of the rectangle """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Public instance method, that returns the rectangle area"""
+        return (self.width * self.heigth)
+
+    def perimeter(self):
+        """Public instance method, that returns the rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width * 2) + (self.height * 2)
