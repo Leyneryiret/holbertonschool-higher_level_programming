f4646441ccc2facce650fbbd918ae09fb221a533
leyneryiret <4704@holbertonschool.com>
2022-06-01 23:26:30 -0500

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,5 +7,7 @@ class LockedClass:
     ycreating new instance attributes,
     except if the new instance attribute is called first_name"""
 
+    __slots__ = ['first_name']
+
     def __init___(self, first_name=""):
         self.first_name = first_name
