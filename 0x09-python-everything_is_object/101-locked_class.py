3e94d59014318082de538de4ed18eb78cf7aa78d
leyneryiret <4704@holbertonschool.com>
2022-06-01 23:24:21 -0500

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,9 +3,9 @@
 
 
 class LockedClass:
-    """special class prevents the user from dynamically creating new instance attributes,
+    """special class prevents the user from dynamically
+    ycreating new instance attributes,
     except if the new instance attribute is called first_name"""
-
 
     def __init___(self, first_name=""):
         self.first_name = first_name
