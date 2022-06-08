1db8884f1546806e621979a9859d20d0d95ddf5d
leyneryiret <4704@holbertonschool.com>
2022-06-07 21:19:16 -0500

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -34,11 +34,9 @@ class Rectangle(BaseGeometry):
         return (self.__width * self.__height)
 
 
-        
 class Square(Rectangle):
     """ class Square that inherits from Rectangle"""
 
-    
     def __init__(self, size):
         """ Builder of the square class """
         super().integer_validator("size", size)
