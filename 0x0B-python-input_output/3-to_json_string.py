394b05ee52f3c36da6accd8a4f581c10f2bf8236
leyneryiret <4704@holbertonschool.com>
2022-06-08 20:23:18 -0500

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  the JSON representation of an object (string) """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """python to json"""
+
+    return json.dumps(my_obj)
