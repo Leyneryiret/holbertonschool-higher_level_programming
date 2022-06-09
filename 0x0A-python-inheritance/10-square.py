6b14bb35d1c2b30fedb1c1f76ddd97762400ca1f
leyneryiret <4704@holbertonschool.com>
2022-06-08 23:29:13 -0500

    up date 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,36 +3,8 @@
 """ Module for geometry class."""
 
 
-class BaseGeometry:
-    """ class BaseGeometry (based on 6-base_geometry.py). """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if (value <= 0):
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ class Rectangle that
-    inherits from BaseGeometry (7-base_geometry.py)."""
-
-    def __init__(self, width, height):
-        """ Builder of the Rectangle class """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
-
-    def area(self):
-        return (self.__width * self.__height)
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """ class Square that inherits from Rectangle"""
