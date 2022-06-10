d0f2ae0f7b8cce107973df3583136007aaeb6435
leyneryiret <4704@holbertonschool.com>
2022-06-09 22:51:05 -0500

    12

--- 0x0B-python-input_output/12-main.py
+++ 0x0B-python-input_output/12-main.py
@@ -4,10 +4,11 @@
 """
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
-def print_triangle():
-        """
+def print_triangle(triangle):
+    """
     Print the triangle
     """
+
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,15 +6,19 @@
 def pascal_triangle(n):
     """pascal_triangle"""
     lis = []
-    nuevo = []
+    z = 0
     if (n <= 0):
         return lis
 
-    for i in range n:
-        colm_nue = len(lis)
-        lis.append(1)
-        for j in range colm_nue:
-            num = nuevo[j] + nuevo[j+1]
-            lis.append = num
-        lis.append(1)
+    for i in range(n):
+        lis.append([])
+
+    for i in range(n):
+        for j in range(i+1):
+            if j == 0 or j == i:
+                lis[i].append(1)
+            else:
+                z = lis[i-1][j] + lis[i-1][j-1]
+                lis[i].append(z)
+
     return lis
